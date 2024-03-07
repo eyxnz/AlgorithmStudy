203a215c06f361fd3c9554157573876f0ac1c7dc
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-03-07 11:55:32 +0900

    [Gold II] Title: 반도체 설계, Time: 2764 ms, Memory: 18916 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int[] arr;
+	static int[] lis; // 최장 증가 부분 수열
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		lis = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			lis[i] = 1; // 자기 자신 가능
+		}
+
+		for(int i = 1; i < N; i++) { // 현재 위치
+			for(int j = 0; j < i; j++) { // 이전 위치
+				if(arr[j] < arr[i]) { // 연결 가능
+					lis[i] = Math.max(lis[i], lis[j] + 1); // 자기 자신 VS j까지 선택 + 자기 자신
+				}
+			}
+		}
+		
+		int max = 0;
+		for(int i = 0; i < N; i++) {
+			max = max > lis[i] ? max : lis[i];
+		}
+		System.out.println(max);
+	}
+}
