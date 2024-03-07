705286f9871fbe54e449cc59fdddc71af04d4ce6
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-03-07 14:17:38 +0900

    [Gold II] Title: 반도체 설계, Time: 188 ms, Memory: 19068 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -5,33 +5,45 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
-	static int[] arr;
-	static int[] lis; // 최장 증가 부분 수열
+	static int[] lis; // 최장 증가 부분 수열 (lis[index] : 길이가 index 인 LIS 중 마지막에 오는 수 저장)
+	static int size;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
 		lis = new int[N];
+		Arrays.fill(lis, N + 1); // 최댓값 + 1 로 설정
+		size = 0;
 		
 		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			lis[i] = 1; // 자기 자신 가능
-		}
-
-		int max = 1;
-		for(int i = 1; i < N; i++) { // 현재 위치
-			for(int j = 0; j < i; j++) { // 이전 위치
-				if(arr[j] < arr[i]) { // 연결 가능
-					lis[i] = Math.max(lis[i], lis[j] + 1); // 자기 자신 VS j까지 선택 + 자기 자신
+		lis[size++] = Integer.parseInt(st.nextToken()); // 첫 번째 수
+		
+		for(int i = 1; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			if(lis[size - 1] < num) {
+				lis[size++] = num;
+				continue;
+			} else if(lis[0] > num) {
+				lis[0] = num;
+				continue;
+			}
+			
+			// 이분 탐색으로 num 이 들어갈 위치 찾기
+			int start = 0, end = size - 1;
+			while(end - start > 1) {
+				int mid = (start + end) / 2;
+				
+				if(lis[mid] > num) {
+					end = mid;
+				} else {
+					start = mid;
 				}
 			}
-			max = max > lis[i] ? max : lis[i];
+			lis[end] = num;
 		}
-
-		System.out.println(max);
+		System.out.println(size);
 	}
 }
