4e3b7bd0d9a8dd14c7d45bf0b7625886f5e7ba6d
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-04-03 09:28:57 +0900

    [Gold IV] Title: 작업, Time: 488 ms, Memory: 60736 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.java
+++ 백준/Gold/2056. 작업/작업.java
@@ -1,67 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
-	static int[] time;
-	static int[] total;
-	static int[] inDegree;
-	static List<Integer>[] pre;
-	static Queue<Integer> queue;
-	static int answer;
+	static int[] dp;
+	static int answer = Integer.MIN_VALUE;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
-		time = new int[N + 1];
-		total = new int[N + 1];
-		inDegree = new int[N + 1];
-		pre = new ArrayList[N + 1];
-		for(int i = 0; i < N + 1; i++) {
-			pre[i] = new ArrayList<>();
-		}
-		queue = new LinkedList<>();
+		dp = new int[N + 1];
 		
-		for(int i = 1; i < N + 1; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			time[i] = Integer.parseInt(st.nextToken()); // 걸리는 시간
+		for(int i = 1; i < N + 1; i++) { // 작업
+			st = new StringTokenizer(br.readLine());
+			int time = Integer.parseInt(st.nextToken());
+			dp[i] = time;
 			
 			int cnt = Integer.parseInt(st.nextToken()); // 선행 관계에 있는 작업들의 개수
-			inDegree[i] += cnt;
-			
-			if(cnt == 0) {
-				queue.offer(i);
-			}
-			
 			for(int j = 0; j < cnt; j++) {
 				int num = Integer.parseInt(st.nextToken()); // 선행 관계에 있는 작업들의 번호
-				
-				pre[num].add(i); // 선행 관계 표시
+				dp[i] = Math.max(dp[i], dp[num] + time);
 			}
-		}
-		
-		while(!queue.isEmpty()) {
-			int work = queue.poll();
 			
-			total[work] += time[work];
-			answer = Math.max(answer, total[work]);
-			
-			for(int another : pre[work]) {
-				inDegree[another]--;
-				
-				total[another] = Math.max(total[another], total[work]);
-				if(inDegree[another] == 0) {
-					queue.offer(another);
-				}
-			}
+			answer = Math.max(answer, dp[i]);
 		}
 		
 		System.out.println(answer);
