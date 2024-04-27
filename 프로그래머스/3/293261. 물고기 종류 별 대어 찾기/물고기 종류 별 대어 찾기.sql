5077b33249407625afecd2cee2a66417aeff9652
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-04-27 19:10:47 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,8 @@
+select id, fish_name, length
+from fish_info join fish_name_info using(fish_type)
+where (fish_type, length) in (
+    select fish_type, max(length)
+    from fish_info
+    group by fish_type
+)
+order by id;
