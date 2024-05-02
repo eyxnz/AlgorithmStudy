5bec26fa50e56f591b7785642f6fccf5ef4bca14
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-05-02 09:25:39 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 596 ms, Memory: 89780 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,17 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int K;
-	static int W, H;
+	static int K, W, H;
 	static int[][] arr;
 	
+	static int[][] monkey = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 	static int[][] horse = {{-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}};
-	static int[][] monkey = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,75 +27,61 @@ public class Main {
 			}
 		}
 		
-		System.out.println(bfs(0, 0));
+		System.out.println(bfs());
 	}
 
-	private static int bfs(int x, int y) {
+	private static int bfs() {
 		Queue<int[]> queue = new LinkedList<>();
-		int[][][] visited = new int[H][W][K + 1];
-		for(int i = 0; i < H; i++) {
-			for(int j = 0; j < W; j++) {
-				Arrays.fill(visited[i][j], -1);
-			}
-		}
+		boolean[][][] visited = new boolean[H][W][K + 1];
 		
-		queue.offer(new int[] {x, y, 0});
-		visited[x][y][0] = 0;
+		queue.offer(new int[] {0, 0, 0, 0});
+		visited[0][0][0] = true;
 		
 		while(!queue.isEmpty()) {
-			int[] info = queue.poll();
-			x = info[0];
-			y = info[1];
-			int z = info[2];
+			int[] now = queue.poll();
+			int x = now[0], y = now[1], k = now[2], cnt = now[3];
 			
 			if(x == H - 1 && y == W - 1) {
-				return visited[x][y][z];
+				return cnt;
 			}
 			
-			if(z < K) { // 말의 움직임으로 이동 가능
+			if(k < K) { // 말의 움직임 가능
 				for(int d = 0; d < horse.length; d++) {
 					int nx = x + horse[d][0], ny = y + horse[d][1];
 					
-					if(!checkArea(nx, ny)) {
+					if(nx < 0 || nx >= H || ny < 0 || ny >= W) { // 범위 밖
 						continue;
 					}
-					if(visited[nx][ny][z + 1] != -1) {
+					if(arr[nx][ny] == 1) { // 장애물
 						continue;
 					}
-					if(arr[nx][ny] != 0) {
+					if(visited[nx][ny][k + 1]) {
 						continue;
 					}
 					
-					queue.offer(new int[] {nx, ny, z + 1});
-					visited[nx][ny][z + 1] = visited[x][y][z] + 1;
+					queue.offer(new int[] {nx, ny, k + 1, cnt + 1});
+					visited[nx][ny][k + 1] = true;
 				}
 			}
 			
 			for(int d = 0; d < monkey.length; d++) {
 				int nx = x + monkey[d][0], ny = y + monkey[d][1];
-					
-				if(!checkArea(nx, ny)) {
+				
+				if(nx < 0 || nx >= H || ny < 0 || ny >= W) { // 범위 밖
 					continue;
 				}
-				if(visited[nx][ny][z] != -1) {
+				if(arr[nx][ny] == 1) { // 장애물
 					continue;
 				}
-				if(arr[nx][ny] != 0) {
+				if(visited[nx][ny][k]) {
 					continue;
 				}
-					
-				queue.offer(new int[] {nx, ny, z});
-				visited[nx][ny][z] = visited[x][y][z] + 1;
+				
+				queue.offer(new int[] {nx, ny, k, cnt + 1});
+				visited[nx][ny][k] = true;
 			}
 		}
 		
 		return -1;
-	}
-
-	private static boolean checkArea(int x, int y) {
-		if(x < 0 || x >= H || y < 0 || y >= W) {
-			return false;
-		}
-		return true;
 	}
 }
