92747cd07a38a98b77f698f482b9c6a0340e62eb
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-07-17 21:50:10 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 844 ms, Memory: 564228 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.java
@@ -1,117 +1,99 @@
 import java.io.*;
 import java.util.*;
 
-class Candy {
-	int amount;
-	int count;
-	
-	public Candy(int amount, int count) {
-		this.amount = amount;
-		this.count = count;
-	}
-
-	@Override
-	public String toString() {
-		return "Candy [amount=" + amount + ", count=" + count + "]";
-	}
-}
-
 public class Main {
 	static int N, M, K;
-	static int[] candy; // 아이들이 받은 사탕의 양
 	static int[] parent; // 집합
-	static int[] setAmount; // 해당 집합의 아이들의 인원 수
-	static int[] setCount; // 해당 집합의 사탕의 양
-	
-	static Candy[] candies;
+	static int[] child; // 각 아이들이 받은 사탕의 수
+	static Map<Integer, int[]> map; // 각 집합이 받은 사탕의 수
+	static int[][] candy;
 	static int[][] dp;
 	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		candy = new int[N + 1];
-		parent = new int[N + 1];
-		setAmount = new int[N + 1];
-		setCount = new int[N + 1];
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 1; i < N + 1; i++) {
-			candy[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i = 0; i < N + 1; i++) {
-			parent[i] = i;
-		}
-		
-		for(int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			unionParent(a, b);
-		}
-		
-		int size = 0;
-		for(int i = 1; i < N + 1; i++) {
-			int p = findParent(i);
-			
-			setAmount[p]++;
-			setCount[p] += candy[i];
-			
-			if(setAmount[p] == 1) {
-				size++;
-			}
-		}
-		
-		int idx = 1;
-		candies = new Candy[size + 1];
-		candies[0] = new Candy(0, 0);
-		for(int i = 1; i < N + 1; i++) {
-			if(setAmount[i] == 0) {
-				continue;
-			}
-			
-			candies[idx++] = new Candy(setAmount[i], setCount[i]);
-		}
-		
-		dp = new int[size + 1][K];
-		for(int i = 1; i < size + 1; i++) {
-			for(int k = 0; k < K; k++) {
-				int amount = candies[i].amount;
-				int count = candies[i].count;
-				
-				if(amount > k) {
-					dp[i][k] = dp[i - 1][k];
-					continue;
-				}
-				
-				dp[i][k] = Math.max(dp[i - 1][k], dp[i - 1][k - amount] + count);
-			}
-		}
-		
-		System.out.println(dp[size][K - 1]);
-	}
-
-	private static int findParent(int x) {
-		if(x == parent[x]) {
-			return parent[x];
-		}
-		return parent[x] = findParent(parent[x]);
-	}
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	
+    	st = new StringTokenizer(br.readLine(), " ");
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	K = Integer.parseInt(st.nextToken());
+    	
+    	parent = new int[N + 1];
+    	child = new int[N + 1];
+    	
+    	st = new StringTokenizer(br.readLine(), " ");
+    	for(int i = 1; i < N + 1; i++) {
+    		parent[i] = i;
+    		child[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	for(int i = 0; i < M; i++) {
+    		st = new StringTokenizer(br.readLine(), " ");
+    		int a = Integer.parseInt(st.nextToken());
+    		int b = Integer.parseInt(st.nextToken());
+    		
+    		unionParent(a, b);
+    	}
+    	
+    	map = new HashMap<>();
+    	
+    	for(int i = 1; i < N + 1; i++) {
+    		int key = findParent(i); // 집합
+    		
+    		if(!map.containsKey(key)) {
+    			map.put(key, new int[] {1, child[i]});
+    		} else {
+    			int[] value = map.get(key);
+    			
+    			map.put(key, new int[] {value[0] + 1, value[1] + child[i]});
+    		}
+    	}
+    	
+    	int index = 1, size = map.size();
+    	candy = new int[size + 1][2];
+    	
+    	for(int key : map.keySet()) {
+    		int[] value = map.get(key);
+    		
+    		candy[index][0] = value[0];
+    		candy[index][1] = value[1];
+    		index++;
+    	}
+    	
+    	dp = new int[size + 1][K];
+    	
+    	for(int i = 1; i < size + 1; i++) {
+    		for(int j = 0; j < K; j++) {
+    			if(candy[i][0] > j) {
+    				dp[i][j] = dp[i - 1][j];
+    				continue;
+    			}
+    			
+    			dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - candy[i][0]] + candy[i][1]);
+    		}
+    	}
+    	
+    	System.out.println(dp[size][K - 1]);
+    }
 
 	private static void unionParent(int a, int b) {
 		a = findParent(a);
 		b = findParent(b);
 		
-		if(a <= b) {
+		if(a == b) {
+			return;
+		} else if(a < b) {
 			parent[b] = a;
 		} else {
 			parent[a] = b;
 		}
+	}
+
+	private static int findParent(int x) {
+		if(x == parent[x]) {
+			return x;
+		}
+		
+		return parent[x] = findParent(parent[x]);
 	}
 }
