4e6b2af3ea9be1a9df1cc1d07b8e5c4212862bb9
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-07-26 23:39:45 +0900

    [Silver I] Title: RGB거리, Time: 92 ms, Memory: 12224 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,47 +1,62 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N; // 2 ≤ N ≤ 1,000
-	static int[][] cost;
-	static int[][] dp;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		cost = new int[N][3];
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			for(int j = 0; j < 3; j++) {
-				cost[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		dp = new int[N][3];
-		for(int i = 0; i < 3; i++) { // 첫 번째 집 칠하기
-			dp[0][i] = cost[0][i];
-		}
-		
-		for(int i = 1; i < N; i++) {
-			// i 번째 집을 빨간색으로 칠할 경우
-			dp[i][0] = dp[i - 1][1] > dp[i - 1][2] ? dp[i - 1][2] : dp[i - 1][1]; // 둘 중 최소값 선택
-			dp[i][0] += cost[i][0];
-			
-			// i 번째 집을 초록색으로 칠할 경우
-			dp[i][1] = dp[i - 1][0] > dp[i - 1][2] ? dp[i - 1][2] : dp[i - 1][0]; // 둘 중 최소값 선택
-			dp[i][1] += cost[i][1];
-			
-			// i 번째 집을 파란색으로 칠할 경우
-			dp[i][2] = dp[i - 1][0] > dp[i - 1][1] ? dp[i - 1][1] : dp[i - 1][0]; // 둘 중 최소값 선택
-			dp[i][2] += cost[i][2];
-		}
-		
-		int answer = dp[N - 1][0] > dp[N - 1][1] ? dp[N - 1][1] : dp[N - 1][0];
-		answer = answer > dp[N - 1][2] ? dp[N - 1][2] : answer;
-		System.out.println(answer);
-	}
+    static int N;
+    static int[][] cost;
+    static int[][][] dp; // [현재 집 인덱스][첫 번째 집 색상][현재 집 색상]
+
+    static int answer = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        cost = new int[N][3];
+        dp = new int[N][3][3];
+        for(int i = 0; i < N; i++) {
+            for(int color = 0; color < 3; color++) {
+                Arrays.fill(dp[i][color], Integer.MAX_VALUE);
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            for(int color = 0; color < 3; color++) {
+                cost[i][color] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[0][0][0] = cost[0][0];
+        dp[0][1][1] = cost[0][1];
+        dp[0][2][2] = cost[0][2];
+
+        dp[1][0][1] = cost[0][0] + cost[1][1];
+        dp[1][0][2] = cost[0][0] + cost[1][2];
+
+        dp[1][1][0] = cost[0][1] + cost[1][0];
+        dp[1][1][2] = cost[0][1] + cost[1][2];
+
+        dp[1][2][0] = cost[0][2] + cost[1][0];
+        dp[1][2][1] = cost[0][2] + cost[1][1];
+
+        for(int i = 2; i < N; i++) {
+            for(int color = 0; color < 3; color++) {
+                dp[i][color][0] = Math.min(dp[i - 1][color][1], dp[i - 1][color][2]) + cost[i][0];
+                dp[i][color][1] = Math.min(dp[i - 1][color][0], dp[i - 1][color][2]) + cost[i][1];
+                dp[i][color][2] = Math.min(dp[i - 1][color][0], dp[i - 1][color][1]) + cost[i][2];
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                answer = Math.min(answer, dp[N - 1][i][j]);
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
