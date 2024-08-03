7db5c1e50ca2991ee7b38b9cfb8c19cc1b93b860
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-08-03 18:37:29 +0900

    [Gold IV] Title: 인구 이동, Time: 496 ms, Memory: 294020 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -2,113 +2,109 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, L, R;
-	static int[][] A;
-	static int[][] tempA;
-	static boolean[][] visited;
-	
-	static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-	
-	static int answer;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		A = new int[N][N];
-		tempA = new int[N][N];
-		
-		for(int r = 0; r < N; r++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			for(int c = 0; c < N; c++) {
-				A[r][c] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		while(true) {
-			int cnt = 0;
-			
-			visited = new boolean[N][N];
-			
-			for(int r = 0; r < N; r++) {
-				for(int c = 0; c < N; c++) {
-					if(visited[r][c]) {
-						continue;
-					}
-					
-					cnt += bfs(r, c);
-				}
-			}
-			
-			if(cnt == 0) {
-				break;
-			}
-			
-			A = tempA;
-			
-			answer++;
-		}
-		
-		System.out.println(answer);
-	}
+    static int N, L, R;
+    static int[][] people; // 각 나라에 사는 인구수
+    static int[][] temp;
+    static int answer;
 
-	private static int bfs(int r, int c) {
-		Queue<int[]> queue = new LinkedList<>();
-		List<int[]> list = new ArrayList<>();
-		int total = 0;
-		
-		queue.offer(new int[] {r, c});
-		visited[r][c] = true;
-		list.add(new int[] {r, c});
-		total += A[r][c];
-		tempA[r][c] = A[r][c];
-		
-		while(!queue.isEmpty()) {
-			int[] now = queue.poll();
-			r = now[0];
-			c = now[1];
-			
-			for(int d = 0; d < dir.length; d++) {
-				int nr = r + dir[d][0], nc = c + dir[d][1];
-				
-				if(nr < 0 || nr >= N || nc < 0 || nc >= N) {
-					continue;
-				}
-				
-				int bound = Math.abs(A[r][c] - A[nr][nc]);
-				if(bound < L || bound > R) {
-					continue;
-				}
-				
-				if(visited[nr][nc]) {
-					continue;
-				}
-				
-				queue.offer(new int[] {nr, nc});
-				visited[nr][nc] = true;
-				list.add(new int[] {nr, nc});
-				total += A[nr][nc];
-			}
-		}
-		
-		if(list.size() == 1) {
-			return 0;
-		}
-		
-		int people = total / list.size();
-		for(int i = 0; i < list.size(); i++) {
-			int[] now = list.get(i);
-			r = now[0];
-			c = now[1];
-			
-			tempA[r][c] = people;
-		}
-		
-		return 1;
-	}
+    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        people = new int[N][N];
+        temp = new int[N][N];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < N; j++) {
+                people[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        while(true) {
+            int move = 0;
+            boolean[][] area = new boolean[N][N];
+
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(area[i][j]) {
+                        continue;
+                    }
+
+                    move += bfs(i, j, area);
+                }
+            }
+            
+            if(move == 0) { // 인구 이동 없음
+                break;
+            }
+
+            people = temp;
+            answer++;
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int bfs(int x, int y, boolean[][] area) {
+        Queue<int[]> queue = new LinkedList<>();
+        Queue<int[]> sameArea = new LinkedList<>(); // 연합에 속한 나라
+        int total = 0; // 연합의 인구수
+
+        queue.offer(new int[] {x, y});
+        sameArea.offer(new int[] {x, y});
+        total += people[x][y];
+        area[x][y] = true;
+
+        while(!queue.isEmpty()) {
+            int[] now = queue.poll();
+            x = now[0];
+            y = now[1];
+
+            for(int d = 0; d < dir.length; d++) {
+                int nx = x + dir[d][0], ny = y + dir[d][1];
+
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                    continue;
+                }
+
+                if(area[nx][ny]) {
+                    continue;
+                }
+
+                int diff = Math.abs(people[x][y] - people[nx][ny]);
+
+                if(diff < L || diff > R) {
+                    continue;
+                }
+
+                queue.offer(new int[] {nx, ny});
+                sameArea.offer(new int[] {nx, ny});
+                total += people[nx][ny];
+                area[nx][ny] = true;
+            }
+        }
+        
+        if(sameArea.size() == 1) {
+            int[] country = sameArea.poll();
+            temp[country[0]][country[1]] = people[country[0]][country[1]];
+
+            return 0;
+        }
+        
+        total = total / sameArea.size(); // 연합을 이루고 있는 각 칸의 인구수
+        while(!sameArea.isEmpty()) {
+            int[] country = sameArea.poll();
+            temp[country[0]][country[1]] = total;
+        }
+
+        return 1;
+    }
 }
