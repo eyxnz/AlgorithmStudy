4898302cc1c682b326b163af534b5900f0100a42
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-08-21 10:55:43 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2332 ms, Memory: 454604 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,64 +1,68 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.io.*;
+import java.util.*;
 
 class Num implements Comparable<Num> {
-	int num;
-	int index;
-	
-	public Num(int num, int index) {
-		this.num = num;
-		this.index = index;
-	}
+    int index;
+    int num;
 
-	@Override
-	public int compareTo(Num o) {
-		if(Integer.compare(num, o.num) == 0) {
-			return Integer.compare(index, o.index); // 이 기준까지 추가해야 TreeSet 에서 중복된 숫자가 들어갈 수 있음
-		}
-		return Integer.compare(num, o.num);
-	}
+    public Num(int index, int num) {
+        this.index = index;
+        this.num = num;
+    }
+
+    @Override
+    public int compareTo(Num o) {
+        if(Integer.compare(num, o.num) == 0) {
+            return Integer.compare(index, o.index);
+        }
+        return Integer.compare(num, o.num); // 오름차순 정렬
+    }
 }
 
 public class Main {
-	static int T;
-	static int K;
-	static TreeSet<Num> set;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		T = Integer.parseInt(br.readLine());
-		for(int t = 0; t < T; t++) {
-			set = new TreeSet<>();
-			K = Integer.parseInt(br.readLine());
-			
-			for(int k = 0; k < K; k++) {
-				st = new StringTokenizer(br.readLine(), " ");
-				String op = st.nextToken();
-				int n = Integer.parseInt(st.nextToken());
-				
-				if(op.equals("I")) { // 삽입
-					set.add(new Num(n, k));
-				} else { // 삭제
-					if(n == -1) { // 최솟값 삭제
-						set.pollFirst();
-					} else { // 최댓값 삭제
-						set.pollLast();
-					}
-				}
-			}
-			
-			if(set.isEmpty()) {
-				sb.append("EMPTY").append("\n");
-			} else {
-				sb.append(set.last().num).append(" ").append(set.first().num).append("\n");
-			}
-		}
-		
-		System.out.print(sb.toString());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t = 0; t < T; t++) {
+            TreeSet<Num> nums = new TreeSet<>();
+            int size = 0;
+
+            int K = Integer.parseInt(br.readLine());
+
+            for(int k = 0; k < K; k++) {
+                st = new StringTokenizer(br.readLine(), " ");
+
+                String operator = st.nextToken();
+                int num = Integer.parseInt(st.nextToken());
+
+                if(operator.equals("D")) {
+                    if(size == 0) {
+                        continue;
+                    }
+
+                    if(num == 1) { // 최댓값 삭제
+                        nums.pollLast();
+                    } else { // 최솟값 삭제
+                        nums.pollFirst();
+                    }
+
+                    size--;
+                } else { // "I"
+                    nums.add(new Num(k, num));
+                    size++;
+                }
+            }
+
+            if(size == 0) {
+                sb.append("EMPTY").append("\n");
+            } else {
+                sb.append(nums.last().num).append(" ").append(nums.first().num).append("\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
 }
