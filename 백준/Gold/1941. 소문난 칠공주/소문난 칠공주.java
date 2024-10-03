f3b43fa180eec2a7790fad5a365632f99d5cb863
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-10-03 17:47:12 +0900

    [Gold III] Title: 소문난 칠공주, Time: 452 ms, Memory: 157096 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,99 +1,108 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N = 5;
-	static int MAX = 7;
-	static int[][] arr;
-	static int answer;
-	
-	static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		arr = new int[N][N];
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-		
-			for(int j = 0; j < N; j++) {
-				if(str.charAt(j) == 'Y') { // 임도연파
-					arr[i][j] = 1;
-				}
-			}
-		}
-		
-		combi(0, 0, 0, 0);
-		
-		System.out.println(answer);
-	}
+    static int N = 5;
+    static int[][] arr;
+    static boolean[][] choice;
 
-	private static void combi(int depth, int start, int s, int y) {
-		if(y >= MAX / 2 + 1) { // 임도연파가 우위를 점할 경우
-			return;
-		}
-		
-		if(depth == MAX) {
-			if(!check(start - 1)) {
-				return;
-			}
-			
-			answer++;
-			return;
-		}
-		
-		for(int i = start; i < N * N; i++) {
-			int r = i / N, c = i % N;
-			int temp = arr[r][c];
-			
-			arr[r][c] = 2;
-			if(temp == 1) {
-				combi(depth + 1, i + 1, s, y + 1);
-			} else {
-				combi(depth + 1, i + 1, s + 1, y);
-			}
-			arr[r][c] = temp;
-		}
-	}
+    static int answer;
 
-	private static boolean check(int point) {
-		int cnt = 0;
-		Queue<int[]> queue = new LinkedList<>();
-		boolean[][] visited = new boolean[N][N];
-		
-		cnt++;
-		queue.offer(new int[] {point / N, point % N});
-		visited[point / N][point % N] = true;
-		
-		while(!queue.isEmpty()) {
-			int[] p = queue.poll();
-			int x = p[0];
-			int y = p[1];
-			
-			for(int d = 0; d < dir.length; d++) {
-				int nx = x + dir[d][0], ny = y + dir[d][1];
-				
-				if(nx < 0 || nx >= N || ny < 0 || ny >= N) {
-					continue;
-				}
-				if(arr[nx][ny] != 2) {
-					continue;
-				}
-				if(visited[nx][ny]) {
-					continue;
-				}
-				
-				cnt++;
-				queue.offer(new int[] {nx, ny});
-				visited[nx][ny] = true;
-			}
-		}
-		
-		if(cnt != MAX) {
-			return false;
-		}
-		return true;
-	}
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        arr = new int[N][N]; // S : 0, Y : 1
+        choice = new boolean[N][N];
+
+        for(int i = 0; i < N; i++) {
+           String temp = br.readLine();
+
+           for(int j = 0; j < N; j++) {
+               char ch = temp.charAt(j);
+
+               if(ch == 'S') {
+                   arr[i][j] = 0;
+               } else {
+                   arr[i][j] = 1;
+               }
+           }
+        }
+
+        permutation(0, -1, 0, 0);
+
+        System.out.println(answer);
+    }
+
+    private static void permutation(int depth, int start, int zero, int one) {
+        if(one > 3) {
+            return;
+        }
+
+        if(depth == 7) {
+            int x = start / N, y = start % N;
+
+            if(isConnected(x, y)) {
+                answer++;
+            }
+
+            return;
+        }
+
+        for(int i = start + 1; i < N * N; i++) {
+            int x = i / N, y = i % N;
+
+            choice[x][y] = true;
+
+            if(arr[x][y] == 0) {
+                permutation(depth + 1, i, zero + 1, one);
+            } else {
+                permutation(depth + 1, i, zero, one + 1);
+            }
+
+            choice[x][y] = false;
+        }
+    }
+
+    private static boolean isConnected(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[N][N];
+        int cnt = 0;
+
+        queue.offer(new int[] {x, y});
+        visited[x][y] = true;
+        cnt++;
+
+        while(!queue.isEmpty()) {
+            int[] now = queue.poll();
+            x = now[0];
+            y = now[1];
+
+            for(int d = 0; d < dir.length; d++) {
+                int nx = x + dir[d][0], ny = y + dir[d][1];
+
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                    continue;
+                }
+
+                if(visited[nx][ny]) {
+                    continue;
+                }
+
+                if(!choice[nx][ny]) {
+                    continue;
+                }
+
+                queue.offer(new int[] {nx, ny});
+                visited[nx][ny] = true;
+                cnt++;
+            }
+        }
+
+        if(cnt < 7) {
+            return false;
+        }
+        return true;
+    }
 }
