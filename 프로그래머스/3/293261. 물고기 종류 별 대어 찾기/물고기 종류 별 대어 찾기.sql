1751abd81501c85980a23080c71c98381e8e7c17
eyxnz <117811664+eyxnz@users.noreply.github.com>
2024-10-18 22:10:39 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -2,7 +2,7 @@ select a.ID, b.FISH_NAME, a.LENGTH
 from FISH_INFO a
 join FISH_NAME_INFO b on a.FISH_TYPE = b.FISH_TYPE
 where (a.FISH_TYPE, a.LENGTH) in (
-    select FISH_TYPE, max(LENGTH)
+    select FISH_TYPE, max(LENGTH) as LENGTH
     from FISH_INFO
     group by FISH_TYPE
 )
